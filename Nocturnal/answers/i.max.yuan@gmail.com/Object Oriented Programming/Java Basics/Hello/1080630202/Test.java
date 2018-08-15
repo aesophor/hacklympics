9b3166e13aca28ca29bd304db087336081c48a45
Aesophor <aesophor@mailfence.com>
2018-08-15 21:19:24 +0800

    Added some screenshots.

--- Nocturnal/answers/i.max.yuan@gmail.com/Object Oriented Programming/Java Basics/Hello/1080630202/Test.java
+++ Nocturnal/answers/i.max.yuan@gmail.com/Object Oriented Programming/Java Basics/Hello/1080630202/Test.java
@@ -1,1 +1,14 @@
-import java.util.Scanner;public class Test {        public static void main(String[] args) {        Scanner cin = new Scanner(System.in);        String s;                while (cin.hasNext()) {            s = cin.nextLine();            System.out.println("hello, " + s);        }    }}
+import java.util.Scanner;
+
+public class Test {
+    
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        String s;
+        
+        while (cin.hasNext()) {
+            s = cin.nextLine();
+            System.out.println("hello, " + s);
+        }
+    }
+}
